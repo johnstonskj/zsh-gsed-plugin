ba497bfaa9936b6b290f55fe572963f52fabf1eb
Simon Johnston <johnstonskj@gmail.com>
2026-01-07 19:59:40 -0800

    docs: add description line

--- gsed.plugin.zsh
+++ gsed.plugin.zsh
@@ -5,14 +5,14 @@
 #
 # Description:
 #
-#   Add one-line description here...
+#   Zsh plugin to replace sed with GNU sed.
 #
 # Public variables:
 #
 # * `GSED`; plugin-defined global associative array with the following keys:
-#   * \`_ALIASES\`; a list of all aliases defined by the plugin.
-#   * \`_FUNCTIONS\`; a list of all functions defined by the plugin.
-#   * \`_PLUGIN_DIR\`; the directory the plugin is sourced from.
+#   * `_ALIASES`; a list of all aliases defined by the plugin.
+#   * `_FUNCTIONS`; a list of all functions defined by the plugin.
+#   * `_PLUGIN_DIR`; the directory the plugin is sourced from.
 #
 
 ############################################################################
